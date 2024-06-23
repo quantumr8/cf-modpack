c2c2b0b3629708a613376cbee68f52a723e40d1e
quantumr8 <sam@rosss.dev>
2024-06-23 16:06:52 -0500

    fix paths

--- update-server.py
+++ update-server.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 with open('/server/updater-config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
+    config = pyyaml.safe_load(file)
 
 # Access values from the config file
 cf_api_url = config['cf_api_url']
@@ -46,7 +46,7 @@ def fetchDownload(project_id,file_id,amp_instance):
     response.raise_for_status()
 
     # Save the file as file_id.zip
-    file_path = f'/server/{amp_instance}/Minecraft/downloads/{file_id}.zip'
+    file_path = f'/server/instances/{amp_instance}/Minecraft/downloads/{file_id}.zip'
     with open(file_path, 'wb') as file:
         file.write(response.content)
     logging.info(f'Downloaded {file_name} from {download_url}')
@@ -88,12 +88,12 @@ def fetchPlugin(id,amp_instance):
     logging.info(f'Founded latest file url: {file_name}')
     download = requests.get(latest_file_url)
     download.raise_for_status()
-    with open(f'/server/{amp_instance}/Minecraft/downloads/{file_name}', 'wb') as file:
+    with open(f'/server/instances/{amp_instance}/Minecraft/downloads/{file_name}', 'wb') as file:
         file.write(download.content)
     logging.debug(f'Downloaded {file_name} from {latest_file_url}')
     sha1 = hashlib.sha1()
     try:
-        with open(f'/server/{amp_instance}/Minecraft/downloads/{file_name}', 'rb') as file:
+        with open(f'/server/instances/{amp_instance}/Minecraft/downloads/{file_name}', 'rb') as file:
             # Read the file in chunks to avoid memory issues with large files
             while chunk := file.read(8192):
                 sha1.update(chunk)
@@ -108,63 +108,63 @@ def fetchPlugin(id,amp_instance):
         logging.info(f'{file_name} downloaded and hash matches')
     else:
         logging.error('File hash does not match')
-        if os.path.exists(f'/server/{amp_instance}/Minecraft/downloads/{file_name}'):
-            os.remove(f'/server/{amp_instance}/Minecraft/downloads/{file_name}')
+        if os.path.exists(f'/server/instances/{amp_instance}/Minecraft/downloads/{file_name}'):
+            os.remove(f'/server/instances/{amp_instance}/Minecraft/downloads/{file_name}')
         return
     logging.debug('Installing plugin')
-    if not os.path.exists(f'/server/{amp_instance}/Minecraft/plugins/{file_name}'):
-        os.makedirs(f'/server/{amp_instance}/Minecraft/plugins', exist_ok=True)
-        shutil.copy(f'/server/{amp_instance}/Minecraft/downloads/{file_name}', f'/server/{amp_instance}/Minecraft/plugins/{file_name}')
+    if not os.path.exists(f'/server/instances/{amp_instance}/Minecraft/plugins/{file_name}'):
+        os.makedirs(f'/server/instances/{amp_instance}/Minecraft/plugins', exist_ok=True)
+        shutil.copy(f'/server/instances/{amp_instance}/Minecraft/downloads/{file_name}', f'/server/instances/{amp_instance}/Minecraft/plugins/{file_name}')
         logging.debug(f'Installed {file_name}')
-        os.remove(f'/server/{amp_instance}/Minecraft/downloads/{file_name}')
+        os.remove(f'/server/instances/{amp_instance}/Minecraft/downloads/{file_name}')
     else:
         logging.debug(f'{file_name} already installed')
-        os.remove(f'/server/{amp_instance}/Minecraft/downloads/{file_name}')
+        os.remove(f'/server/instances/{amp_instance}/Minecraft/downloads/{file_name}')
 
 def installFiles(amp_instance,file_path):
     # Move files matching the regex to a temp folder
     logging.info('Moving files to temp folder')
-    os.makedirs(f'/server/{amp_instance}/Minecraft/temp/mods', exist_ok=True)
-    os.makedirs(f'/server/{amp_instance}/Minecraft/temp/config', exist_ok=True)
+    os.makedirs(f'/server/instances/{amp_instance}/Minecraft/temp/mods', exist_ok=True)
+    os.makedirs(f'/server/instances/{amp_instance}/Minecraft/temp/config', exist_ok=True)
     mods_patterns = ["BlueMap*", "bmm*", "Chunky*", "dcintergration*", "HuskHomes*", "InvView*", "ledger*", "LuckPerms*", "minimotd*", "tabtps*", "worldedit*"]
     config_patterns = ["BlueMap*", "Chunky*", "dcintergration*", "HuskHomes*", "Discord*", "ledger*", "LuckPerms*", "minimotd*", "tabtps*", "worldedit*", "do_a_barrel_roll-server*"]
 
     for pattern in mods_patterns:
-        for file in glob.glob(f'/server/{amp_instance}/Minecraft/mods/{pattern}'):
-            destination = os.path.join(f'/server/{amp_instance}/Minecraft/mods', os.path.basename(file))
+        for file in glob.glob(f'/server/instances/{amp_instance}/Minecraft/mods/{pattern}'):
+            destination = os.path.join(f'/server/instances/{amp_instance}/Minecraft/mods', os.path.basename(file))
             shutil.move(file, destination)
 
     for pattern in config_patterns:
-        for file in glob.glob(f'/server/{amp_instance}/Minecraft/config/{pattern}'):
-            destination = os.path.join('/server/{amp_instance}/Minecraft/config', os.path.basename(file))
+        for file in glob.glob(f'/server/instances/{amp_instance}/Minecraft/config/{pattern}'):
+            destination = os.path.join('/server/instances/{amp_instance}/Minecraft/config', os.path.basename(file))
             shutil.move(file, destination)
 
     # Delete the old mods folder and the config folder:
     logging.info('Deleting old mods and config folders')
-    if os.path.exists(f'/server/{amp_instance}/Minecraft/mods'):
-        os.system(f'rm -rf /server/{amp_instance}/Minecraft/mods')
-    if os.path.exists('/server/{amp_instance}/Minecraft/config'):
-        os.system(f'rm -rf /server/{amp_instance}/Minecraft/config')
+    if os.path.exists(f'/server/instances/{amp_instance}/Minecraft/mods'):
+        os.system(f'rm -rf /server/instances/{amp_instance}/Minecraft/mods')
+    if os.path.exists('/server/instances/{amp_instance}/Minecraft/config'):
+        os.system(f'rm -rf /server/instances/{amp_instance}/Minecraft/config')
 
     # Unzip the file if it's a zip file
     logging.info('Unzipping file')
     if file_path.endswith('.zip'):
         with zipfile.ZipFile(file_path, 'r') as zip_ref:
-            zip_ref.extractall(f'/server/{amp_instance}/Minecraft/')
+            zip_ref.extractall(f'/server/instances/{amp_instance}/Minecraft/')
 
     # Move the contents of temp/mods and temp/config into the /server/Minecraft/mods and /server/Minecraft/config folders
     logging.info('Moving temp/mods and temp/config into Minecraft folder')
-    for file in glob.glob(f'./server/{amp_instance}/Minecraft/temp/mods/*'):
-        shutil.move(file, f'/server/{amp_instance}/Minecraft/mods')
-    for file in glob.glob(f'/server/{amp_instance}/Minecraft/temp/config/*'):
-        shutil.move(file, f'/server/{amp_instance}/Minecraft/config')
+    for file in glob.glob(f'./server/instances/{amp_instance}/Minecraft/temp/mods/*'):
+        shutil.move(file, f'/server/instances/{amp_instance}/Minecraft/mods')
+    for file in glob.glob(f'/server/instances/{amp_instance}/Minecraft/temp/config/*'):
+        shutil.move(file, f'/server/instances/{amp_instance}/Minecraft/config')
 
     # Remove the zip file and the temp folder
     logging.info('Removing zip file and temp folder')
     if os.path.exists(file_path):
         os.remove(file_path)
-    if os.path.exists(f'/server/{amp_instance}/Minecraft/temp'):
-        os.system(f'rm -rf /server/{amp_instance}/Minecraft/temp')
+    if os.path.exists(f'/server/instances/{amp_instance}/Minecraft/temp'):
+        os.system(f'rm -rf /server/instances/{amp_instance}/Minecraft/temp')
 
 # Main
 app = Flask(__name__)
@@ -180,12 +180,12 @@ def update():
         server_file_id = fetchServerPack(project_id)
         logging.info(f'Latest file ID: {server_file_id}')
         # Download the file if it doesn't exist
-        if not os.path.exists(f'/server/{amp_instance}/Minecraft/downloads/{server_file_id}.zip'):
+        if not os.path.exists(f'/server/instances/{amp_instance}/Minecraft/downloads/{server_file_id}.zip'):
             logging.info('Downloading...')
             file_path = fetchDownload(project_id,server_file_id,amp_instance)
         else:
             logging.info('File already exists')
-            file_path = f'/server/{amp_instance}/Minecraft/downloads/{server_file_id}.zip'
+            file_path = f'/server/instances/{amp_instance}/Minecraft/downloads/{server_file_id}.zip'
         logging.info('Installing files')
         installFiles(amp_instance,file_path)
         logging.info('Sending ntfy message')
