a6f248dad19fc03fcd756c909059fcaf2b5cfd44
quantumr8 <sam@rosss.dev>
2024-06-23 15:13:16 -0500

    update paths again

--- update-server.py
+++ update-server.py
@@ -47,7 +47,7 @@ def fetchDownload(project_id,file_id):
     response.raise_for_status()
 
     # Save the file as file_id.zip
-    file_path = f'downloads/{file_id}.zip'
+    file_path = f'../downloads/{file_id}.zip'
     with open(file_path, 'wb') as file:
         file.write(response.content)
     logging.info(f'Downloaded {file_name} from {download_url}')
@@ -90,12 +90,12 @@ def fetchPlugin(id):
     logging.info(f'Founded latest file url: {file_name}')
     download = requests.get(latest_file_url)
     download.raise_for_status()
-    with open(f'downloads/{file_name}', 'wb') as file:
+    with open(f'../downloads/{file_name}', 'wb') as file:
         file.write(download.content)
     logging.debug(f'Downloaded {file_name} from {latest_file_url}')
     sha1 = hashlib.sha1()
     try:
-        with open(f'downloads/{file_name}', 'rb') as file:
+        with open(f'../downloads/{file_name}', 'rb') as file:
             # Read the file in chunks to avoid memory issues with large files
             while chunk := file.read(8192):
                 sha1.update(chunk)
@@ -110,63 +110,63 @@ def fetchPlugin(id):
         logging.info(f'{file_name} downloaded and hash matches')
     else:
         logging.error('File hash does not match')
-        if os.path.exists(f'downloads/{file_name}'):
-            os.remove(f'downloads/{file_name}')
+        if os.path.exists(f'../downloads/{file_name}'):
+            os.remove(f'../downloads/{file_name}')
         return
     logging.debug('Installing plugin')
-    if not os.path.exists(f'plugins/{file_name}'):
-        os.makedirs('plugins', exist_ok=True)
-        shutil.copy(f'downloads/{file_name}', f'plugins/{file_name}')
+    if not os.path.exists(f'../plugins/{file_name}'):
+        os.makedirs('../plugins', exist_ok=True)
+        shutil.copy(f'../downloads/{file_name}', f'../plugins/{file_name}')
         logging.debug(f'Installed {file_name}')
-        os.remove(f'downloads/{file_name}')
+        os.remove(f'../downloads/{file_name}')
     else:
         logging.debug(f'{file_name} already installed')
-        os.remove(f'downloads/{file_name}')
+        os.remove(f'../downloads/{file_name}')
 
 def installFiles(file_path):
     # Move files matching the regex to a temp folder
     logging.info('Moving files to temp folder')
-    os.makedirs('temp/mods', exist_ok=True)
-    os.makedirs('temp/config', exist_ok=True)
+    os.makedirs('../temp/mods', exist_ok=True)
+    os.makedirs('../temp/config', exist_ok=True)
     mods_patterns = ["BlueMap*", "bmm*", "Chunky*", "dcintergration*", "HuskHomes*", "InvView*", "ledger*", "LuckPerms*", "minimotd*", "tabtps*", "worldedit*"]
     config_patterns = ["BlueMap*", "Chunky*", "dcintergration*", "HuskHomes*", "Discord*", "ledger*", "LuckPerms*", "minimotd*", "tabtps*", "worldedit*", "do_a_barrel_roll-server*"]
 
     for pattern in mods_patterns:
-        for file in glob.glob(f'mods/{pattern}'):
+        for file in glob.glob(f'../mods/{pattern}'):
             destination = os.path.join('mods', os.path.basename(file))
             shutil.move(file, destination)
 
     for pattern in config_patterns:
-        for file in glob.glob(f'config/{pattern}'):
+        for file in glob.glob(f'../config/{pattern}'):
             destination = os.path.join('config', os.path.basename(file))
             shutil.move(file, destination)
 
     # Delete the old mods folder and the config folder:
     logging.info('Deleting old mods and config folders')
-    if os.path.exists('mods'):
-        os.system('rm -rf mods')
-    if os.path.exists('config'):
-        os.system('rm -rf config')
+    if os.path.exists('../mods'):
+        os.system('rm -rf ../mods')
+    if os.path.exists('../config'):
+        os.system('rm -rf ../config')
 
     # Unzip the file if it's a zip file
     logging.info('Unzipping file')
     if file_path.endswith('.zip'):
         with zipfile.ZipFile(file_path, 'r') as zip_ref:
-            zip_ref.extractall('./')
+            zip_ref.extractall('../')
 
     # Move the contents of temp/mods and temp/config into the /server/Minecraft/mods and /server/Minecraft/config folders
     logging.info('Moving temp/mods and temp/config into Minecraft folder')
-    for file in glob.glob('temp/mods/*'):
-        shutil.move(file, 'mods')
-    for file in glob.glob('temp/config/*'):
-        shutil.move(file, 'config')
+    for file in glob.glob('../temp/mods/*'):
+        shutil.move(file, '../mods')
+    for file in glob.glob('../temp/config/*'):
+        shutil.move(file, '../config')
 
     # Remove the zip file and the temp folder
     logging.info('Removing zip file and temp folder')
     if os.path.exists(file_path):
         os.remove(file_path)
-    if os.path.exists('temp'):
-        os.system('rm -rf temp')
+    if os.path.exists('../temp'):
+        os.system('rm -rf ../temp')
 
 # Main
 app = Flask(__name__)
@@ -181,12 +181,12 @@ def update():
         server_file_id = fetchServerPack(project_id)
         logging.info(f'Latest file ID: {server_file_id}')
         # Download the file if it doesn't exist
-        if not os.path.exists(f'./{server_file_id}.zip'):
+        if not os.path.exists(f'../downloads/{server_file_id}.zip'):
             logging.info('Downloading...')
             file_path = fetchDownload(project_id,server_file_id)
         else:
             logging.info('File already exists')
-            file_path = f'/server/{server_file_id}.zip'
+            file_path = f'../downloads/{server_file_id}.zip'
         logging.info('Installing files')
         installFiles(file_path)
         logging.info('Sending ntfy message')
